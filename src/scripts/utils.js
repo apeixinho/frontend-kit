cce1869d241aaebf68444b61dd11924697357641
apeixinho <a_peixinho@sapo.pt>
2018-07-08 14:20:27 +0200

    Wrong removal off onscroll event. Added back again

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,35 +3,41 @@ import scrollSpy from './scrollspy';
 // on document ready
 document.addEventListener('DOMContentLoaded', function () {
 
-  // window is greater than 561px
-  if (typeof document.documentElement != 'undefined' &&
-    typeof document.documentElement.clientWidth !=
-    'undefined' && document.documentElement.clientWidth > 561) {
+  //on window scroll
+  window.onscroll = function () {
+    // window is greater than 561px
+    if (typeof document.documentElement != 'undefined' &&
+      typeof document.documentElement.clientWidth !=
+      'undefined' && document.documentElement.clientWidth > 561) {
 
-    let topHeader = document.querySelector('header');
-    let topHeaderHeight = topHeader.offsetHeight;
-    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    // if scroll position is greater than header height add sticky class   
-    if (scrollPosition > topHeaderHeight) {
-      topHeader.classList.add("sticky");
-    } else {
-      topHeader.classList.remove("sticky");
+      let topHeader = document.querySelector('header');
+      let topHeaderHeight = topHeader.offsetHeight;
+      let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+      // if scroll position is greater than header height add sticky class   
+      if (scrollPosition > topHeaderHeight) {
+        topHeader.classList.add("sticky");
+      } else {
+        topHeader.classList.remove("sticky");
+      }
     }
-  }
+  };
 
-  // window is less than 561px, add click listeners to anchors
-  if (typeof document.documentElement != 'undefined' &&
-    typeof document.documentElement.clientWidth !=
-    'undefined' && document.documentElement.clientWidth < 561) {
-    var item_list = document.querySelector('.nav-container').getElementsByTagName('a');
-    for (var i = 0; i < item_list.length; i++) {
-      item_list[i].addEventListener('click', function () {
-        document.getElementById('menu-nav').checked = false;
-        // remove sticky class
-        document.querySelector('.nav-container').classList.remove("sticky");
-      }, false);
+  // on window resize
+  window.onresize = function () {
+    // window is less than 561px, add click listeners to anchors
+    if (typeof document.documentElement != 'undefined' &&
+      typeof document.documentElement.clientWidth !=
+      'undefined' && document.documentElement.clientWidth < 561) {
+      var item_list = document.querySelector('.nav-container').getElementsByTagName('a');
+      for (var i = 0; i < item_list.length; i++) {
+        item_list[i].addEventListener('click', function () {
+          document.getElementById('menu-nav').checked = false;
+          // remove sticky class
+          document.querySelector('.nav-container').classList.remove("sticky");
+        }, false);
+      }
     }
-  }
+  };
 
   // add click listeners to anchors
   document.getElementById('menu-nav').addEventListener('click', function () {
